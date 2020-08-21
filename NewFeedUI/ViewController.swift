5723ac683cc05a47951ee903f6801060c10aa785
LTT Nguyen Hoang Son <sonnh@lifetimetech.vn>
2020-08-21 09:40:10 +0700

    Initial Commit

---
+++ NewFeedUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewFeedUI
+//
+//  Created by tuanna on 8/21/20.
+//  Copyright © 2020 Le Duy Anh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
