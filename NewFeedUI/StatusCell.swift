c2dc85f902dde093f1b012381e351cb101f7d728
LTT Nguyen Hoang Son <sonnh@lifetimetech.vn>
2020-08-21 17:24:30 +0700

    first commit

---
+++ NewFeedUI/StatusCell.swift
@@ -0,0 +1,17 @@
+//
+//  StatusCell.swift
+//  NewFeedUI
+//
+//  Created by tuanna on 8/21/20.
+//  Copyright © 2020 Le Duy Anh. All rights reserved.
+//
+
+import UIKit
+
+class StatusCell: UITableViewCell {
+
+    @IBOutlet weak var statusImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
--- NewFeedUI/ViewController.swift
+++ NewFeedUI/ViewController.swift
@@ -9,12 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var statusTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUp()
     }
-
-
+    
+    func setUp() {
+        
+        //statusTableView.register(StatusCell.self, forCellReuseIdentifier: "statusCell")
+        statusTableView.register(UINib(nibName: "StatusCell", bundle: nil), forCellReuseIdentifier: "statusCell")
+        statusTableView.dataSource = self
+        statusTableView.delegate = self
+    }
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //        guard let cell = tableView.dequeueReusableCell(withIdentifier: "statusCell", for: indexPath) as? StatusCell  else {
+        //            fatalError("Error")
+        //        }
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "statusCell", for: indexPath) as? StatusCell else {
+            return UITableViewCell()
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        guard let postDetailViewController = storyboard?.instantiateViewController(identifier: "PostDetailViewController") as? PostDetailViewController else {
+//            return
+//        }
+        
+        guard let postDetailViewController = storyboard?.instantiateViewController(withIdentifier: "PostDetailViewController") as? PostDetailViewController else {
+            return
+        }
+        navigationController?.pushViewController(postDetailViewController, animated: true)
+    }
+}
